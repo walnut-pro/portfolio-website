e6adcdbb76e521ddb40554cbf889164f39d5b8bd
Takumu Yoshiba <mukuta273.pooh@gmail.com>
2022-07-31 22:33:43 +0900

    delete &apros and modify sentence

--- src/components/template/IntroduceTemplate.tsx
+++ src/components/template/IntroduceTemplate.tsx
@@ -10,12 +10,11 @@ const IntroduceTemplate = () => {
       </Box>
       <Box>
         <Text>
-          I&apros;m an Application Engineer working in Tokyo. I always develop products with a
-          passion for helping people. When I was a child, I had a disease called epilepsy, and
-          I&apors;ve spent a lot of time in anxiety. Now, I am fully recovered and I have a strong
-          desire to empower people who need to help through the power of technology. Currently,
-          I&apors;m interested in web and iOS application engineering, education, and medical
-          engineering.
+          I am an Application Engineer working in Tokyo. I always develop products with a passion
+          for helping people. When I was a child, I had a disease called epilepsy, and I have spent
+          a lot of time in anxiety. Now, I am fully recovered and I have a strong desire to empower
+          people who need to help through the power of technology. Currently, I am interested in web
+          and iOS application engineering, education, and medical engineering.
         </Text>
       </Box>
     </VStack>
